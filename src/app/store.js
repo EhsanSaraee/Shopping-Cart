aa6ee0b465a047d7d636a18668e8e51ea93570c4
ehsan <ehsansaraee6570@yahoo.com>
2022-04-11 12:51:39 +0430

    fetching data with async thunk

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { productsFetcher } from '../features/productsSlice';
 import reducer from './rootReducer';
 
 export const store = configureStore({
    reducer,
 });
+
+store.dispatch(productsFetcher());
--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -1,14 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
    items: [],
    status: null,
 };
 
+export const productsFetcher = createAsyncThunk(
+   'products/productsFetcher',
+   async () => {
+      const { data } = await axios.get('http://localhost:5000/products');
+      return data;
+   }
+);
+
 export const productsSlice = createSlice({
    name: 'products',
    initialState,
    reducers: {},
+   extraReducers: {
+      [productsFetcher.pending]: (state) => {
+         state.status = 'loading';
+      },
+      [productsFetcher.fulfilled]: (state, action) => {
+         state.items = action.payload;
+         state.status = 'succeeded';
+      },
+      [productsFetcher.rejected]: (state, action) => {
+         state.status = 'failed';
+      },
+   },
 });
 
 export default productsSlice.reducer;
