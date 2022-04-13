7de743d191d0b00b59902b3bf56de52fa63bc1a1
ehsan <ehsansaraee6570@yahoo.com>
2022-04-13 17:23:50 +0430

    display items in cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,98 @@
+import { useSelector } from 'react-redux';
+import {
+   CardContainer,
+   CardEmpty,
+   CartItem,
+   CartItems,
+   CartPrice,
+   CartProduct,
+   CartQuantity,
+   Count,
+   Price,
+   ProductTitle,
+   Quantity,
+   StartShopping,
+   Titles,
+   Total,
+   TotalPrice,
+   Amount,
+   CartCheckout,
+   CartSummary,
+   ClearButton,
+   ContinueShopping,
+   SubTotal,
+} from './styles';
+import { Link } from 'react-router-dom';
+import { ArrowLeftIcon } from '../../components/Icons';
+
 const Cart = () => {
+   const { cartItems, cartTotalAmount } = useSelector((state) => state.cart);
+
    return (
-      <>
-         <h1>Cart</h1>
-      </>
+      <CardContainer>
+         <h2>Shopping Cart</h2>
+         {cartItems.length === 0 ? (
+            <CardEmpty>
+               <p>Your car is currently empty</p>
+               <StartShopping>
+                  <Link to="/">
+                     <ArrowLeftIcon />
+                     <span>Start Shopping</span>
+                  </Link>
+               </StartShopping>
+            </CardEmpty>
+         ) : (
+            <>
+               <Titles>
+                  <ProductTitle>Product</ProductTitle>
+                  <Price>Price</Price>
+                  <Quantity>Quantity</Quantity>
+                  <Total>Total</Total>
+               </Titles>
+               <CartItems>
+                  {cartItems &&
+                     cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id}>
+                           <CartProduct>
+                              <img src={cartItem.image} alt={cartItem.name} />
+                              <div>
+                                 <h3>{cartItem.name}</h3>
+                                 <p>{cartItem.desc}</p>
+                                 <button>Remove</button>
+                              </div>
+                           </CartProduct>
+                           <CartPrice>${cartItem.price}</CartPrice>
+                           <CartQuantity>
+                              <button>-</button>
+                              <Count>{cartItem.cartQuantity}</Count>
+                              <button>+</button>
+                           </CartQuantity>
+                           <TotalPrice>
+                              ${cartItem.price * cartItem.cartQuantity}
+                           </TotalPrice>
+                        </CartItem>
+                     ))}
+               </CartItems>
+               <CartSummary>
+                  <ClearButton>Clear Cart</ClearButton>
+                  <CartCheckout>
+                     <SubTotal>
+                        <span>Subtotal</span>
+                        <Amount>${cartTotalAmount}</Amount>
+                     </SubTotal>
+                     <p>Taxes and shipping calculated at checkout</p>
+                     <button>Check out</button>
+                     <ContinueShopping>
+                        <Link to="/">
+                           <ArrowLeftIcon />
+                           <span>Continue Shopping</span>
+                        </Link>
+                     </ContinueShopping>
+                  </CartCheckout>
+               </CartSummary>
+            </>
+         )}
+      </CardContainer>
    );
 };
 
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.section``;
+
+export const CardEmpty = styled.section``;
+
+export const StartShopping = styled.div``;
+
+export const Titles = styled.section``;
+
+export const ProductTitle = styled.h3``;
+
+export const Price = styled.h3``;
+
+export const Quantity = styled.h3``;
+
+export const Total = styled.h3``;
+
+export const CartItems = styled.section``;
+
+export const CartItem = styled.div``;
+
+export const CartProduct = styled.div``;
+
+export const CartPrice = styled.div``;
+
+export const CartQuantity = styled.div``;
+
+export const Count = styled.div``;
+
+export const TotalPrice = styled.div``;
+
+export const CartSummary = styled.section``;
+
+export const ClearButton = styled.button``;
+
+export const CartCheckout = styled.section``;
+
+export const SubTotal = styled.div``;
+
+export const Amount = styled.span``;
+
+export const ContinueShopping = styled.div``;
